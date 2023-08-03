d1fef3898768b54ce81a256e96e118901f3b0d4a
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-08-03 17:55:38 +0330

    vue-bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,26 @@
 <template>
   <div>
-    <FallowerUsers :followers="followers" @toRemoveFollower="removeFollower($event)" />
+    <b-alert variant="success" show>Succes Alert</b-alert>
+    <b-alert variant="danger" show>Succes Alert</b-alert>
+    <b-alert variant="info" show>Succes Alert</b-alert>
+    <b-alert variant="warning" show>Succes Alert</b-alert>
   </div>
 </template>
 
 <script>
 
-import FallowerUsers from './components/FallowerUsers.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import {BAlert} from 'bootstrap-vue'
 
 export default {
   name: 'App',
-  data () {
-    return {
-      followers : [
-        {id : 1 , name :"amin"},
-        {id : 2 , name :"amir"},
-        {id : 3 , name :"ali"},
-        {id : 4 , name :"ahmad"},
-        {id : 5 , name :"hasan"},
-      ]
-    }
-  },
-  methods : {
-    removeFollower (id) {
-      let followerIndex = this.followers.findIndex( follower => follower.id===id) ;
-      console.log(followerIndex)
-      this.followers.splice(followerIndex,1)
-    }
-  },
   components : {
-    FallowerUsers
+    BAlert
   }
+
+
   }
 
 
--- src/components/FallowerUsers.vue
+++ src/components/FallowerUsers.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
-      <ul>
-        <li v-for="follower in followers" :key="follower.id">
-        <h2>{{ follower.name }}</h2>
-        <button @click="sendFollower(follower.id)">Remove Follower</button>
-        <hr>
-      </li>
-      </ul>
+
     </div>
 </template>
 
@@ -15,13 +9,6 @@
 
 export default {
   name: 'FallowerUsers' ,
-  props : ['followers'] ,
-  methods : {
-    sendFollower (id) {
-      // console.log(id)
-      this.$emit("toRemoveFollower",id)
-    }
-  }
 }
 
 
