637c0fc72850564361e06bcea1f4e37890fbf2f4
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-08-03 11:03:48 +0330

    nested components

---
+++ src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>
+        {{ title }}
+    </h1>
+</template>
+
+
+<script>
+export default {
+    name : "userBox" ,
+    props : ['title'],
+
+}
+</script>
+
+
+
+
+<style>
+
+</style>
