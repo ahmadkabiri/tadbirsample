6bf80fe6e68aebaaa8c770ce4091fb98c001fcbe
AhmadKabiri <51478462+ahmadkabiri@users.noreply.github.com>
2023-08-03 12:16:19 +0330

    how virtual dom works

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div>
-    <userBox  title="AMIR"/>
-    <userBox  title="AMIN"/>
-    <userBox  title="ALI"/>
-    <userBox  title="HASAN"/>
-    <userBox  title="QADIR"/>
-    <h1>User From App Component</h1>
-  </div>
+<div>
+  <h1>{{ title }}</h1>
+  <button @click="changeTitle">Change Title</button>
+  <hr/>
+  <UserName/>
+</div>
 </template>
 
 <script>
-import userBox from './components/User.vue';
+
+import UserName from './UserName.vue';
 
 export default {
   name: 'App',
   components : {
-    userBox
-  }
+    UserName
+  },
+  data () {
+    return{
+      title : "Sabzlearn.ir"
+    }
+  },
+  methods : {
+    changeTitle () {
+      this.title = 'Vue Js Course'
+    }
+  },
+  beforeUpdate () {
+  
+  },
+  updated () {
+    console.log("App" , "Update Shodam! :)")
 
-}
+  },
+  mounted () {
+    console.log("App" , "Mount Shodam ! :)")
+  }
+  }
 
 
 </script>
 
-<style>
-
+<style scoped>
+div{
+  text-align: center;
+  
+}
 </style>
---
+++ src/UserName.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1>
+        User Component
+    </h1>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'UserName',
+  updated () {
+    console.log("User" , "Update Shodam ! :)")
+  },
+  mounted () {
+    console.log("User" , "Mount Shodam ! :)")
+    
+  }
+}
+
+
+</script>
+
+<style scoped>
+div{
+    text-align: center;
+}
+</style>
